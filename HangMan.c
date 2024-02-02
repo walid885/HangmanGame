6d43184ede43a91d0d82eb057c636e8a191215dd
walid885 <bettaieb88walid@gmail.com>
2024-02-02 22:35:11 +0100

    trying to the  work on the hangman visualization

--- HangMan.c
+++ HangMan.c
@@ -1,5 +1,17 @@
 #include<stdio.h>
+#include "headers.c/HangmanTermInt.h"
 
-void main (){
-    printf("hello world! ");
+int  main (){
+    char body[7] = "      "; // Initialize the body array with spaces
+    int mistakes = 0;
+    char guess[] = "example"; // Replace this with the actual guessed word
+
+    // Call the printBody function to display the initial body
+    printBody(mistakes, body);
+
+    // Call the printWord function to display the initial guessed word
+    printWord(guess, strlen(guess));
+
+
+   return 0 ; 
 }
