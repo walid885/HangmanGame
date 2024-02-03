83388a6b0145cb88905edcfe61270f7e615cde40
walid885 <bettaieb88walid@gmail.com>
2024-02-03 14:47:39 +0100

    reordering the different files and directories

--- HangMan.c
+++ HangMan.c
@@ -1,16 +1,6 @@
 #include<stdio.h>
-#include "headers.c/HangmanTermInt.h"
 
 int  main (){
-    char body[7] = "      "; // Initialize the body array with spaces
-    int mistakes = 0;
-    char guess[] = "example"; // Replace this with the actual guessed word
-
-    // Call the printBody function to display the initial body
-    printBody(mistakes, body);
-
-    // Call the printWord function to display the initial guessed word
-    printWord(guess, strlen(guess));
 
 
    return 0 ; 
