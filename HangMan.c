fcb8e30d60c9b6f67d0615bc90ef6413c72b0679
walid885 <bettaieb88walid@gmail.com>
2024-02-03 16:41:43 +0100

    testing the various functionalities of the tree manipulation and it is compiling

--- HangMan.c
+++ HangMan.c
@@ -1,7 +1,30 @@
 #include<stdio.h>
+#include "headers/struct.h"
+#include "headers/tree.h"
 
-int  main (){
 
+int main() {
+    // Create an empty tree
+    tree *myTree = arbreConsVide();
 
-   return 0 ; 
+    // Construct a tree node
+    tree *leftSubtree = arbreConsVide();
+    tree *rightSubtree = arbreConsVide();
+    tree *root = arbreCons('A', 2, leftSubtree, rightSubtree);
+
+    // Access the properties of the tree node
+    char letter = arbreRacineLetter(root);
+    int occurrences = arbreRacineNbOcc(root);
+
+    // Access the left and right subtrees
+    tree *left = arbreFilsGauche(root);
+    tree *right = arbreFilsDroit(root);
+
+    // Check if the tree is empty
+    int isEmpty = arbreEstVide(root);
+   printf("Success!");
+    // Delete the tree
+    arbreSuppr(root);
+
+    return 0;
 }
