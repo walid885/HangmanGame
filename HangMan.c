af7a76f365fdc56e252f483e9b83a32361594cc3
walid885 <bettaieb88walid@gmail.com>
2024-02-03 22:44:18 +0100

    prboelm in the Hangman file , it is not running correctly , tested with output messgs, but it is not working

--- HangMan.c
+++ HangMan.c
@@ -5,30 +5,57 @@
 
 int main() {
     // Create an empty tree
+    printf("main is running ! "); 
     tree *myTree = arbreConsVide();
 
-    // Open the file containing the words
-    FILE *fp;
-    fp = fopen("../src/Words/Animals/level1.txt", "r");
+    // Check if the tree is empty
+    if (arbreEstVide(myTree)) {
+        // Handle the empty tree case, e.g., create a new root
+        // Example: myTree = createNewRoot();
+    } else {
+        // Continue with the existing tree operations
+        // Open the file containing the words
+        FILE *fp;
+        fp = fopen("../src/Words/Animals/level1.txt", "r");
 
-    // Read each word from the file and insert it into the tree
-    char word[50];
-    while (fscanf(fp, "%s", word) != EOF) {
-        myTree = dicoInsererMot(word, myTree, 0);
+        // Read each word from the file and insert it into the tree
+        char word[50];
+        while (fscanf(fp, "%s", word) != EOF) {
+            myTree = dicoInsererMot(word, myTree, 0);
+        }
+
+        // Close the file
+        fclose(fp);
+        printf("is this working ? \n");
+        fflush(stdout); // Force the message to be displayed
+
+        // Display all the words in the tree
+        char wordArray[50];
+        dicoAfficher(myTree, wordArray, 0);
+printf("Words in the tree:\n");
+dicoAfficher(myTree, wordArray, 0);
+
+// Delete the tree
+arbreSuppr(myTree);
+
+//char word[50];
+printf("Words from the file:\n");
+while (fscanf(fp, "%s", word) != EOF) {
+    printf("%s\n", word);  // Print the word
+    myTree = dicoInsererMot(word, myTree, 0);
+}
+
+
+
+
+        // Delete the tree
+        arbreSuppr(myTree);
     }
-
-    // Close the file
-    fclose(fp);
-
-    // Display all the words in the tree
-    char wordArray[50];
-    dicoAfficher(myTree, wordArray, 0);
-
-    // Delete the tree
-    arbreSuppr(myTree);
 
     return 0;
 }
+
+
 
 
 
